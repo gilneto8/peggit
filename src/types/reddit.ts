e4c0ce9bc7a6bbb5820f199a2c12a787e5353b54
Gil Neto <gilneto8@gmail.com>
2025-02-16 16:41:27 +0000

    linted.

--- src/types/reddit.ts
+++ src/types/reddit.ts
@@ -1,5 +1,3 @@
-import { RedditUser } from 'snoowrap';
-
 export interface RedditClientInterface {
   validateCredentials(username: string, password: string): Promise<boolean>;
   // submitPost(subreddit: string, title: string, text: string): Promise<string>;
